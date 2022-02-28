c0bad591f5add697dc666f5cb188aa5dcb27034c
Smitesh Patil <59860322+smiteshpatil@users.noreply.github.com>
2022-02-28 09:21:03 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,14 @@
+# In python Print() function is used to display the output in the terminal
+
+print("Hello, World")
+
+#here, "" inside the print function indicates that its a string
+
+a=123
+print(a)
+#here, we don't need to use "" because it is a variable & will be treated as a code.
+
+
+# The output Will be
+HEllo, World
+123
